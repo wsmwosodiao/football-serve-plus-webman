29bbefa57d8d402adbdc802c8c50bfdc0f9a7af8
user <cny1236166@gmail.com>
2022-10-26 14:43:24 +0800

    x 26-7

--- app/Services/RobotPushUserService.php
+++ app/Services/RobotPushUserService.php
@@ -182,7 +182,7 @@ class RobotPushUserService extends BaseService
                     $params['country']=$footBallFixturePush->country;
                 }
                 $params['bot_name']=$this->bot_name;//强制设置机器人为55data
-                Log::info("推送赛事给用户-标题",["params"=>$params,"referral_code"=>$referral_code]);
+                //Log::info("推送赛事给用户-标题",["params"=>$params,"referral_code"=>$referral_code]);
                 if($this->is_push){
                     $this->httpWorkerman->post($this->pushUserUrl, $params);
                 }
@@ -203,10 +203,10 @@ class RobotPushUserService extends BaseService
                             $params['country']=$footBallFixturePush->country;
                         }
                         $params['bot_name']=$this->bot_name;//强制设置机器人为55data
-                        Log::info("推送赛事给用户-图片",["params"=>$params,"referral_code"=>$referral_code,'url'=>$this->pushUserUrl,"is_Debug"=>$this->is_Debug]);
+                        //Log::info("推送赛事给用户-图片",["params"=>$params,"referral_code"=>$referral_code,'url'=>$this->pushUserUrl,"is_Debug"=>$this->is_Debug]);
                         if($this->is_push){
                             $res=$this->httpWorkerman->post($this->pushUserUrl, $params);
-                            Log::info("推送赛事给用户-图片-返回",["res"=>$res,"referral_code"=>$referral_code]);
+                            //Log::info("推送赛事给用户-图片-返回",["res"=>$res,"referral_code"=>$referral_code]);
                         }
                         $i++;
                     }
@@ -229,13 +229,15 @@ class RobotPushUserService extends BaseService
                     $params['country']=$footBallFixturePush->country;
                 }
                 $params['bot_name']=$this->bot_name;//强制设置机器人为55data
-                Log::info("推送赛事给用户-简介",["params"=>$params,"referral_code"=>$referral_code]);
+                //Log::info("推送赛事给用户-简介",["params"=>$params,"referral_code"=>$referral_code]);
                 if($this->is_push){
                     $this->httpWorkerman->post($this->pushUserUrl, $params);
                 }
             }
+        }else{
+            Log::info("推送赛事给用户--数据不存在",["userRobotSubscribe_id"=>$userRobotSubscribe_id,"referral_code"=>$referral_code]);
         }
-        Log::info("推送赛事给用户--数据不存在",["userRobotSubscribe_id"=>$userRobotSubscribe_id,"referral_code"=>$referral_code]);
+
     }
 
 
