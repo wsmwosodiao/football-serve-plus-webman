816f492ff1f3a769d17efae63956d602d60ba39c
user <cny1236166@gmail.com>
2022-10-26 10:06:19 +0800

    x 26-1

--- process/Task.php
+++ process/Task.php
@@ -15,7 +15,7 @@ class Task
     public function onWorkerStart()
     {
         // 每1小时第2分执行【获取24小时赛事图片】
-        new Crontab('2 */3 * * *', function(){
+        new Crontab('*/120 * * * *', function(){
             RobotPushService::make()->getTodayMacth();
             echo "获取24小时赛事图片：".date('Y-m-d H:i:s')."\n";
         });
