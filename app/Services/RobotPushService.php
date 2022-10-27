f56bf92c0a2493c187b2d6ab93b9668cc4a95507
user <cny1236166@gmail.com>
2022-10-27 20:56:26 +0800

    x 27-28

--- app/Services/RobotPushService.php
+++ app/Services/RobotPushService.php
@@ -404,7 +404,7 @@ class RobotPushService extends BaseService
             ->whereNotIn('order_sn', $order_sns)
             ->where('created_at', ">" ,Carbon::now()->subMinutes(20))
             ->with(['user'])->lazyById(10)->each(function ($item) use (&$count,$footBallFixturePushAll) {
-                    if(Carbon::make($item->created_at)->addMinutes(10)->lt(Carbon::now())){
+                    if(Carbon::make($item->created_at)->addMinutes(2)->lt(Carbon::now())){
                         $count ++;
                         $key=$item->user->local;
                         $text=data_get($footBallFixturePushAll, "config_".$key,"");
