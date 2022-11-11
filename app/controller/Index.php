854a79e8e09d420e34aa4c85cbe6b9e2b3cd9095
jason <tyyun0128@gmail.com>
2022-11-11 18:19:09 +0800

    dd

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -26,7 +26,7 @@ class Index
         $id = $request->input('id');
         $url = $request->input('url');
         $footBallAfterImgPush = FootBallAfterImgPush::query()->where('_id',$id)->first();
-        $post_data = $footBallAfterImgPush->post_data;
+        $post_data = data_get($footBallAfterImgPush,'post_data');
         if($footBallAfterImgPush) {
             Log::info('$footBallAfterImgPush',[$footBallAfterImgPush]);
             RobotPushService::make()->pushSend($post_data->footBallFixturePushAll,$post_data->content,$post_data->language,$post_data->key,$post_data->referral_code,true,$url);
