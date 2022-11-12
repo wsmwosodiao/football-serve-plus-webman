8036019cfcafdac8907e4fc8b10b1bcb3649ac74
jason <tyyun0128@gmail.com>
2022-11-12 10:26:24 +0800

    dd

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ use support\Request;
 return [
     'debug' => true,
     'error_reporting' => E_ALL,
-    'default_timezone' => 'UTC',
+    'default_timezone' => 'Asia/Shanghai',
     'request_class' => Request::class,
     'public_path' => base_path() . DIRECTORY_SEPARATOR . 'public',
     'runtime_path' => base_path(false) . DIRECTORY_SEPARATOR . 'runtime',
