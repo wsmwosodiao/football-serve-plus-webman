af2f182036e3448e2bef7be01491069519aa3e92
jason <tyyun0128@gmail.com>
2022-11-13 16:34:26 +0800

    dd

--- process/Task.php
+++ process/Task.php
@@ -91,12 +91,12 @@ class Task
         });
 //* 10/20 * * *
         // 每20分钟执行一次
-        new Crontab('1 * 20/1 * * *', function(){
+        new Crontab('1 20 20/1 * * *', function(){
             RobotPushService::make()->pushDeposit('group1');
             echo "充值推送：".date('Y-m-d H:i:s')."\n";
         });
         // 每20分钟执行一次
-        new Crontab('1 * 20/11 * * *', function(){
+        new Crontab('1 20 20/11 * * *', function(){
             RobotPushService::make()->pushDeposit('group2');
             echo "活动推送：".date('Y-m-d H:i:s')."\n";
         });
